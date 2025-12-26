8db3b34545b70596adf0c9d105961b7cf8d0e828
Danila Eliseev <danila.eliseev@innowise.com>
2025-12-26 16:07:38 +0100

    Add initial files for Lab4, including a detailed description of the competitive neural network, a report template, and a basic C++ solution structure. This sets the foundation for implementing and documenting the lab work.

---
+++ Lab4/solution.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+#include <random>
+#include <algorithm>
+#include <chrono>
+#include <cmath>
+
+using namespace std;
+
+// TODO: Добавьте необходимые константы и структуры данных
+
+// TODO: Реализуйте необходимые функции
+
+int main() {
+    setlocale(LC_ALL, "Russian");
+    
+    // TODO: Реализуйте основную логику лабораторной работы
+    
+    return 0;
+}
+
